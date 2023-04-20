083c2bd9ff3795530f053a9e52727252b8cea6e6
Francesco Carlo Mantegazza <francesco.mantegazza@uni-graz.at>
2023-04-20 10:51:25 +0200

    do not expose the module

--- py_tutorials/my_test_case.py
+++ py_tutorials/my_test_case.py
@@ -1,44 +1,52 @@
 # solve the Poisson equation -Delta u = f
 # with Dirichlet boundary condition u = 0
 
-from ngsolve import *
+import ngsolve as ng
 from netgen.geom2d import unit_square
 import numpy as np
 
-ngsglobals.msg_level = 1
+ng.ngsglobals.msg_level = 1
 
 # generate a triangular mesh of mesh-size 0.2
-mesh = Mesh(unit_square.GenerateMesh(maxh=0.2))
+mesh = ng.Mesh(unit_square.GenerateMesh(maxh=0.2))
 
 # H1-conforming finite element space
-fes = H1(mesh, order=3, dirichlet=[1,2,3,4])
+fes = ng.H1(mesh, order=3, dirichlet=[1,2,3,4])
 
 # define trial- and test-functions
 u = fes.TrialFunction()
 v = fes.TestFunction()
 
 # the right hand side
-f = LinearForm(fes)
-f += 32 * (y*(1-y)+x*(1-x)) * v * dx
+f = ng.LinearForm(fes)
+def g(x):
+    return ng.exp(x**2 / 2)
+
+#print("g(x) = ", g(np.linspace(0,10,100)))
+
+f += 32 * g(ng.x) * v * ng.dx
 
 # the bilinear-form 
-a = BilinearForm(fes, symmetric=True)
-a += grad(u)*grad(v)*dx
-a += u*v*dx
+a = ng.BilinearForm(fes, symmetric=True)
+a += ng.grad(u)*ng.grad(v)*ng.dx
+a += u*v*ng.dx
 
 a.Assemble()
 f.Assemble()
 
 # the solution field 
-gfu = GridFunction(fes)
+gfu = ng.GridFunction(fes)
 gfu.vec.data = a.mat.Inverse(fes.FreeDofs(), inverse="sparsecholesky") * f.vec
-print(type(gfu.vec))
-
-
+print(type(gfu.vec), type(gfu.vec.data))
+array = np.empty(0)
+array = np.append(gfu.vec.data.FV().NumPy(), array)
+print(type(array), array.shape)
+array = np.append(gfu.vec.data.FV().NumPy(), array)
+print(type(array), array.shape)
 
 # plot the solution (netgen-gui only)
-Draw (gfu)
-Draw (-grad(gfu), mesh, "Flux")
+ng.Draw (gfu)
+ng.Draw (-ng.grad(gfu), mesh, "Flux")
 
-exact = 16*x*(1-x)*y*(1-y)
-print ("L2-error:", sqrt (Integrate ( (gfu-exact)*(gfu-exact), mesh)))
+exact = 16 * ng.x * (1-ng.x) * ng.y * (1-ng.y)
+print ("L2-error:", ng.sqrt (ng.Integrate ( (gfu-exact)*(gfu-exact), mesh)))
